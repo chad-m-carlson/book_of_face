c85d092920e9de14e69987293b3bc96b1c5bc1b8
Chad <chad.m.carlson@gmail.com>
2019-06-21 13:43:33 -0600

    comments now have author, custom sql added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,14 @@
 class Comment < ApplicationRecord
   belongs_to :user
+
+  def self.persons_comments(person_id)
+    Comment.find_by_sql(
+      "SELECT body, person_id, user_id, name
+      FROM comments
+      LEFT JOIN users as u
+      ON user_id = u.id
+      WHERE person_id = #{person_id}
+      "
+    )
+  end
 end
